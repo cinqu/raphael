9ab96ad8b72b28a12539b6c9b01ec392799dbcbd
Tom Streller <dusklykan@yahoo.co.uk>
2011-03-07 23:53:39 +0100

    sbt tweaks

--- project/build/Raphael.scala
+++ project/build/Raphael.scala
@@ -3,6 +3,9 @@ import sbt._
 class RaphaelProject(info: ProjectInfo) extends DefaultProject(info) {
   override def shouldCheckOutputDirectories = false
 
+  override def compileOptions = super.compileOptions ++ Seq(Optimize, target(Target.Java1_5))
+  override def packageOptions = super.packageOptions ++ Seq(MainClass("scan.apps.raphael.App"))
+
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
   val scalaActors = "org.scala-lang" % "scala-actors" % "2.4.0-RC2"
   val h2Database = "com.h2database" % "h2" % "1.3.148"
